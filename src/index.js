4333235e30f32e05ea9a3580442fe0b4ed1231ce
getYourElementById <zhyf0310@163.com>
2019-08-31 21:27:10 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import combineReducers from './reduxUtils/combineReducers'
 import createReducer from './createReducer'
 import promiseMiddleware from './reduxPromiseMiddleware'
 
+import AsyncComponent from './asyncComponent'
+import SpaAssyProvider from './spaassyProvider'
+import SpaAssyConnect from './spaassyConnect'
+import SpaAssyRegister from './spaassyRegister'
 
 module.exports = {
     bindActionCreators,
     combineReducers,
     createReducer,
-    promiseMiddleware
+    promiseMiddleware,
+
+    AsyncComponent,
+    SpaAssyProvider,
+    SpaAssyConnect,
+    SpaAssyRegister
 }
